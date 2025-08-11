3aa81390e3e3ee02cc9235c29e91c7bd00a638a8
Gabriel N <spbiel18@gmail.com>
2025-08-11 13:00:18 -0300

    busca binaria

--- Python/buscaSimples/busca_binaria.py
+++ Python/buscaSimples/busca_binaria.py
@@ -2,11 +2,17 @@ def buscaBinaria (vet, x):
     inicio = 0
     fim = len(vet) - 1
     while inicio <= fim:
-        meio = (inicio+fim)/2
+        meio = (inicio+fim) // 2
 
         if vet[meio] > x:
             fim = meio - 1
         elif vet[meio] < x:
             inicio = meio + 1
         else:
+            return meio
+    return -1
+
+vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(buscaBinaria(vetor, 9))
             
--- Python/buscaSimples/busca_simples.py
+++ Python/buscaSimples/busca_simples.py
@@ -12,3 +12,7 @@ def buscaWhile(vet, x):
         return -1
     else:
         return i
+
+vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(buscaWhile(vetor, 3))
